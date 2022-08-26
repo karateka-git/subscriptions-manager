27ce37fd0ea48fa880e738b4ad24223777bf4f33
vlatrof <gthbttr@gmail.com>
2022-08-26 12:52:05 +0300

    creating root destination

---
+++ app/app/src/main/java/com/vlatrof/subscriptionsmanager/presentation/rootactivity/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.vlatrof.subscriptionsmanager.presentation.rootactivity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+    }
+
+}
