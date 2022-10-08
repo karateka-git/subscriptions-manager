c4d1af44de45bf277fe07228f53d3b34a3aa7cb1
vlatrof <gthbttr@gmail.com>
2022-10-08 17:01:29 +0300

    improving adding new subscription screen

--- app/app/src/main/java/com/vlatrof/subscriptionsmanager/presentation/fragments/NewSubscriptionFragment.kt
+++ app/app/src/main/java/com/vlatrof/subscriptionsmanager/presentation/fragments/NewSubscriptionFragment.kt
@@ -7,6 +7,7 @@ import android.text.TextWatcher
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.datepicker.MaterialDatePicker
@@ -39,17 +40,31 @@ class NewSubscriptionFragment : Fragment(R.layout.fragment_new_subscription) {
     }
 
     private fun setupGoBackButton() {
-        binding.btnGoBack.setOnClickListener{
+        binding.btnNewSubscriptionGoBack.setOnClickListener{
             hideKeyboard()
             findNavController().popBackStack()
         }
     }
 
     private fun setupCostInput() {
-        binding.tietNewSubscriptionCost.setText("0.00")
+
+        val costField = binding.tietNewSubscriptionCost
+
+        // setup default value
+        costField.setText("0.00")
+
+        // setup default value if lose focus with no entered value
+        costField.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus && costField.text!!.isEmpty()) {
+                binding.tietNewSubscriptionCost.setText("0.00")
+            }
+        }
+
     }
 
     private fun setupStartDateInput() {
+
+        val dateField = binding.tietNewSubscriptionStartDate
 
         // init date picker instance
         datePicker = MaterialDatePicker
@@ -60,21 +75,23 @@ class NewSubscriptionFragment : Fragment(R.layout.fragment_new_subscription) {
             .build()
 
         datePicker.addOnPositiveButtonClickListener {
-            binding.tietNewSubscriptionStartDate.setText(datePicker.headerText)
+            dateField.setText(datePicker.headerText)
         }
 
-        binding.tietNewSubscriptionStartDate.setOnClickListener{
+        dateField.setOnClickListener{
             datePicker.show(parentFragmentManager, "datePicker")
         }
 
-        // set current date as default value
+        // setup current date as default value
         val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("d MMM yyyy")
         val formattedString = LocalDate.now().format(formatter)
-        binding.tietNewSubscriptionStartDate.setText(formattedString)
+        dateField.setText(formattedString)
 
     }
 
     private fun setupAlertsInput() {
+
+        val alertsField = binding.actvNewSubscriptionAlerts
 
         val availableAlerts = arrayOf(
             "None",
@@ -90,36 +107,27 @@ class NewSubscriptionFragment : Fragment(R.layout.fragment_new_subscription) {
             availableAlerts
         )
 
-        binding.alertsSpinner.setAdapter(alertsAdapter)
+        alertsField.setAdapter(alertsAdapter)
 
-        // set default selection to "none"
+        // setup default selection to "none"
         val defaultValue = "None"
-        binding.alertsSpinner.setText(defaultValue, false)
+        alertsField.setText(defaultValue, false)
 
     }
 
     private fun setupCreateButton() {
-        binding.btnCreateNewSubscription.setOnClickListener{
 
-            val allFilled = false
+        binding.btnNewSubscriptionSubmit.setOnClickListener{
 
-            // check are all required inputs filled
-
-            if(!allFilled) {
-                Toast.makeText(requireActivity(),
-                    "Not all inputs filled", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-
-            Toast.makeText(requireActivity(),
-                "fdafdasfdsfsadfas", Toast.LENGTH_SHORT).show()
+            // todo
 
         }
     }
 
     private fun setupCurrencyInput() {
 
-        val input = binding.currenciesSpinner
+        val currencyFieldContainer = binding.tilNewSubscriptionCurrency
+        val currencyField = binding.actvNewSubscriptionCurrency
 
         // setup fill currencies spinner with values
         val availableCurrencies = Currency.getAvailableCurrencies().toTypedArray()
@@ -129,35 +137,35 @@ class NewSubscriptionFragment : Fragment(R.layout.fragment_new_subscription) {
             android.R.layout.simple_spinner_dropdown_item,
             availableCurrencies
         )
-        input.setAdapter(currenciesAdapter)
+        currencyField.setAdapter(currenciesAdapter)
 
         // setup default selection to "USD"
         val defaultValue = "USD"
-        input.setText(defaultValue, false)
+        currencyField.setText(defaultValue, false)
 
         // setup force only capital characters
-        input.addTextChangedListener(object : TextWatcher {
+        currencyField.addTextChangedListener(object : TextWatcher {
             override fun onTextChanged(arg0: CharSequence, arg1: Int, arg2: Int, arg3: Int) {}
             override fun beforeTextChanged(arg0: CharSequence, arg1: Int, arg2: Int, arg3: Int) {}
             override fun afterTextChanged(et: Editable) {
                 var s = et.toString()
                 if (s != s.uppercase(Locale.getDefault())) {
                     s = s.uppercase(Locale.getDefault())
-                    input.setText(s)
-                    input.setSelection(input.length())
+                    currencyField.setText(s)
+                    currencyField.setSelection(currencyField.length())
                 }
             }
         })
 
         // setup input validation after lose focus
-        input.setOnFocusChangeListener { v, hasFocus ->
+        currencyField.setOnFocusChangeListener { v, hasFocus ->
 
             // skip if get focus
             if (hasFocus) return@setOnFocusChangeListener
 
             // show error if entered currency is empty
-            if (binding.currenciesSpinner.text.toString().isEmpty()) {
-                binding.tilNewSubscriptionCurrency.error =
+            if (currencyField.text.toString().isEmpty()) {
+                currencyFieldContainer.error =
                     getString(R.string.til_new_subscription_currency_error_empty_currency)
                 return@setOnFocusChangeListener
             }
@@ -165,26 +173,28 @@ class NewSubscriptionFragment : Fragment(R.layout.fragment_new_subscription) {
             // show error if entered currency code don't available
             try {
                 if (!availableCurrencies.contains(
-                        Currency.getInstance(binding.currenciesSpinner.text.toString()))
+                        Currency.getInstance(currencyField.text.toString()))
                 ) {
-                    binding.tilNewSubscriptionCurrency.error =
+                    currencyFieldContainer.error =
                         getString(R.string.til_new_subscription_currency_error_wrong_currency)
                     return@setOnFocusChangeListener
                 }
             } catch (exception: IllegalArgumentException) {
-                binding.tilNewSubscriptionCurrency.error =
+                currencyFieldContainer.error =
                     getString(R.string.til_new_subscription_currency_error_wrong_currency)
                 return@setOnFocusChangeListener
             }
 
             // else clear error
-            binding.tilNewSubscriptionCurrency.error = ""
+            currencyFieldContainer.error = ""
 
         }
 
     }
 
     private fun setupRenewalPeriodInput() {
+
+        val renewalPeriodField = binding.actvNewSubscriptionRenewalPeriod
 
         val availablePeriods = arrayOf(
             "Daily",
@@ -202,11 +212,11 @@ class NewSubscriptionFragment : Fragment(R.layout.fragment_new_subscription) {
             availablePeriods
         )
 
-        binding.renewalPeriodsSpinner.setAdapter(periodsAdapter)
+        renewalPeriodField.setAdapter(periodsAdapter)
 
         // set default selection to "monthly"
         val defaultValue = "Monthly"
-        binding.renewalPeriodsSpinner.setText(defaultValue, false)
+        renewalPeriodField.setText(defaultValue, false)
 
     }
 
