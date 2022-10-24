2bed87c4e067af1f46c6058f71c004dbdf04d981
vlatrof <gthbttr@gmail.com>
2022-10-24 20:06:50 +0300

    fixed navigation animations

---
+++ app/app/src/main/java/com/vlatrof/subscriptionsmanager/presentation/utils/NotificationHelper.kt
@@ -0,0 +1,10 @@
+package com.vlatrof.subscriptionsmanager.presentation.utils
+
+import android.content.Context
+
+class NotificationHelper(private val context: Context) {
+
+    private val channelId = "REMINDER_CHANNEL_ID"
+    private val notificationId = 1
+
+}
