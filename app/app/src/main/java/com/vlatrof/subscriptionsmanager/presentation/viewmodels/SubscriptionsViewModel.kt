6ba7e9b57f4a15370128ae17dd517296b473fa89
vlatrof <gthbttr@gmail.com>
2022-10-30 18:24:50 +0300

    did some code cleaning

--- app/app/src/main/java/com/vlatrof/subscriptionsmanager/presentation/viewmodels/SubscriptionsViewModel.kt
+++ app/app/src/main/java/com/vlatrof/subscriptionsmanager/presentation/viewmodels/SubscriptionsViewModel.kt
@@ -3,16 +3,8 @@ package com.vlatrof.subscriptionsmanager.presentation.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewModelScope
 import com.vlatrof.subscriptionsmanager.domain.models.Subscription
-import com.vlatrof.subscriptionsmanager.domain.usecases.interfaces.DeleteAllSubscriptionsUseCase
 import com.vlatrof.subscriptionsmanager.domain.usecases.interfaces.GetAllSubscriptionsUseCase
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.transform
-import kotlinx.coroutines.launch
 
 class SubscriptionsViewModel(
 
